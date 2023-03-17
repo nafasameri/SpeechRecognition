6bf2122cc6022ed4718c1e4b9597e7898e6267d9
Nafas <nafiseh.ameri@partsoftware.com>
2023-03-17 15:01:02 +0330

    add feature translator

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import speech_recognition as sr
+from translate import Translator
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print('Listening...')
-    r.pause_threshold = 1
-    audio = r.listen(source)
+lang = 'fa-IR'
+from_lang = 'Persian'
+to_lang = 'English'
 
-    try:
-        print('Recognizing...')
-        query = r.recognize_google(audio, language ='en-US', show_all=False)
-        print('List of words:')
-        print(query.__str__().split(' '))
-    except Exception as e:
-        print(e)
-        print('Unable to Recognize your voice.')
+while True:
+    print('')
+    from_lang, lang = ('English', 'en-US') if lang == 'fa-IR' else ('Persian', 'fa-IR')
+    to_lang = 'Persian' if to_lang == 'English' else 'English'
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Speak in', from_lang)
+        print('Listening...')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+        try:
+            print('Recognizing...')
+
+            query = r.recognize_google(audio, language=lang)
+            print(from_lang, ':', query)
+
+            translator = Translator(from_lang=from_lang, to_lang=to_lang)
+            translation = translator.translate(query)
+            print(to_lang, ':', translation)
+
+        except Exception as e:
+            print('Unable to Recognize your voice; because:', e)
